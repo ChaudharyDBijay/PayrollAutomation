4d5f696d394ba3f8bed52b385d26f4a3ebb82ef9
ChaudharyDBijay <bijay.chaudhary@8squarei.com>
2024-05-19 20:30:49 +0545

    changing the payroll SIT environment URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   use: {
     headless: false,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://release-dev.payroll-3gl.pages.dev/",
+    baseURL: "https://payroll-3gl.pages.dev/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
