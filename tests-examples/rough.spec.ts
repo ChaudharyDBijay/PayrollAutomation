d3beba7dcb8e287a749f955a60eb76298ade4480
ChaudharyDBijay <bijay.chaudhary@8squarei.com>
2024-05-19 20:40:43 +0545

    This is test for branch

--- tests-examples/rough.spec.ts
+++ tests-examples/rough.spec.ts
@@ -1,17 +1,50 @@
-import { url } from "inspector";
-import { test } from "../page-object-models/base.ts";
+import { expect, test } from "../page-object-models/base.ts";
 
 test.describe("This is to verify active Employee", () => {
-  test.beforeEach(async ({ loginPage }) => {
+  test.use({ baseURL: "https://reqres.in/api" });
+
+  test.only("API testing demo", async ({ request }) => {
+    const response = await request.get("/users?page=2");
+try {
+    const responseBody = JSON.parse(await response.text());
+    console.log(responseBody);
+} catch (error) {
+    // Handle parsing error here, log or throw as needed
+    console.error("Error parsing response body:", error);
+}
+  });
+
+  test("GET Request", async ({ request }) => {
+    const response = await request.get("/users/1");
+    const responseBody = JSON.parse(await response.text());
+    expect(response.status()).toBe(200);
+    expect(responseBody.data.id).toBe(1);
+    expect(responseBody.data.first_name).toContain("ge");
+    console.log(responseBody.data.first_name);
+  });
+
+  test.skip("POST request", async ({ request }) => {
+    const response = await request.post("/users", {
+      data: { name: "breazy", job: "brodin" },
+    });
+    const body = await response.text()
+    var responseBody = JSON.parse(body);
+    console.log(responseBody);
+  });
+
+  test.skip("this is before method", async ({ loginPage }) => {
     await loginPage.login("payroll@grr.la", "Admin@123");
   });
 
-  test("Verifying Active Employee", async ({ page, activeEmployeePage }) => {
+  test.skip("Verifying Active Employee", async ({
+    page,
+    activeEmployeePage,
+  }) => {
     await activeEmployeePage.navigateToActiveEmployeePage();
     const adduserapireqpromise = page.waitForResponse((request) => {
       return (
         request.url() ===
-          "https://payrolldev.vanillatech.asia/api/OnBoardingPayroll/activeemployee/init" 
+        "https://payrolldev.vanillatech.asia/api/OnBoardingPayroll/activeemployee/init"
       );
     });
     await console.log("==>" + adduserapireqpromise);
